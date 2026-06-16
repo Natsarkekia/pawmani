e2f48462f5dcd247377ac585311f0da8669e7137
Giorgi <giorgikshb@gmail.com>
2026-06-16 17:28:35 +0400

    Pull Supabase hostname from env

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "yykevfztaszwbwwfdxrx.supabase.co",
+        hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL!).hostname,
       },
     ],
   },
